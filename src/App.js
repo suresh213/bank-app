606ee107a9ed14efd39dc0f6eeea57f6c35ebe93
suresh213 <asuresh52012@gmail.com>
2022-12-19 00:12:13 +0530

    Add. Features based on requirements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,142 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { v4 } from "uuid";
+import {
+  accounts,
+  initailNewAccountData,
+  initialAccountDatas,
+} from "./constants";
+import "./App.css";
 
 function App() {
+  const [accountData, setAccountData] = useState(initialAccountDatas);
+  const [newAccountData, setNewAccountData] = useState(initailNewAccountData);
+  const [totalAmount, setTotalAmount] = useState(0);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    setTotalAmount(calculateTotalAmount());
+  }, [accountData]);
+
+  const showError = (msg) => {
+    setError(msg);
+    setTimeout(() => {
+      setError("");
+    }, 2000);
+  };
+
+  const handleSubmit = (e) => {
+    e?.preventDefault();
+
+    if (newAccountData.accountId == "-1") {
+      showError("Please select an account");
+      return;
+    }
+
+    setAccountData([
+      ...accountData,
+      {
+        ...newAccountData,
+        transactionId: v4(),
+      },
+    ]);
+
+    setNewAccountData(initailNewAccountData);
+  };
+
+  const handleDelete = (transactionId) => {
+    const alteredAccountData = accountData.filter(
+      (account) => account.transactionId != transactionId
+    );
+
+    setAccountData(alteredAccountData);
+  };
+
+  const displayInIndianRupeeFormat = (amount) => {
+    return amount.toLocaleString("en-IN");
+  };
+
+  const calculateTotalAmount = () => {
+    const totalAmount = accountData.reduce((result, data) => {
+      result += Number(data.creditAmount);
+      result -= Number(data.debitAmount);
+      return result;
+    }, 0);
+
+    return totalAmount;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form-outer-div">
+        <form onSubmit={handleSubmit}>
+          <select
+            value={newAccountData.accountId}
+            onChange={(e) =>
+              setNewAccountData({
+                ...newAccountData,
+                accountId: e.target.value,
+              })
+            }
+          >
+            <option value={"-1"}>Select account</option>
+            {accounts.map((account, index) => (
+              <option value={account.id} key={index}>
+                {account.name}
+              </option>
+            ))}
+          </select>
+          <input
+            placeholder="Credit amount"
+            value={newAccountData.creditAmount}
+            onChange={(e) =>
+              setNewAccountData({
+                ...newAccountData,
+                creditAmount: e.target.value,
+              })
+            }
+          />
+          <input
+            placeholder="Debit amount"
+            value={newAccountData.debitAmount}
+            onChange={(e) =>
+              setNewAccountData({
+                ...newAccountData,
+                debitAmount: e.target.value,
+              })
+            }
+          />
+          <button type="submit">Add</button>
+        </form>
+      </div>
+      {error?.length > 0 && <p className="error">{error}</p>}
+
+      <table>
+        <thead>
+          <tr>
+            <th>Account</th>
+            <th>Credit amount</th>
+            <th>Debit amount</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {accountData.map((account, index) => (
+            <tr key={index}>
+              <td>
+                {accounts.find((a) => a.id == account.accountId)?.name || ""}
+              </td>
+              <td>{displayInIndianRupeeFormat(account.creditAmount)}</td>
+              <td>{displayInIndianRupeeFormat(account.debitAmount)}</td>
+              <td>
+                <button onClick={() => handleDelete(account.transactionId)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <p>Total amount : {displayInIndianRupeeFormat(totalAmount)}</p>
     </div>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,38 @@
+export const accounts = [
+  {
+    id: 1,
+    name: "HDFC Bank",
+  },
+  {
+    id: 2,
+    name: "State Bank of India",
+  },
+];
+
+export const initailNewAccountData = {
+  transactionId: null,
+  accountId: "-1",
+  creditAmount: 0,
+  debitAmount: 0,
+};
+
+export const initialAccountDatas = [
+  {
+    transactionId: "645dc492-3f18-4706-9d8b-252c6269538c",
+    accountId: 2,
+    creditAmount: 5000,
+    debitAmount: 3000,
+  },
+  {
+    transactionId: "21ce0d46-3446-4dae-981d-a852f783567f",
+    accountId: 1,
+    creditAmount: 6000,
+    debitAmount: 5000,
+  },
+  {
+    transactionId: "0aa340a1-2c37-48ed-9f19-904ad444d9b8",
+    accountId: 1,
+    creditAmount: 200000,
+    debitAmount: 5000,
+  },
+];
